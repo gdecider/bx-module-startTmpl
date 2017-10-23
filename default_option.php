1a99b796d35786e6f594582477e45d08e8b737c0
Denis M <gdecider@gmail.com>
2017-10-23 15:30:29 +0300

    Create default_option.php

---
+++ default_option.php
@@ -0,0 +1,8 @@
+<?php if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
+
+$arModConf = include __DIR__ . '/mod_conf.php';
+
+$local_exch1c_default_option = array(
+    //$arModConf['name'] . '_SOME_VAR_NAME' => "some default var value",    
+);
+?>
