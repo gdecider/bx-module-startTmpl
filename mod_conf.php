92b8e153d3467dff85884e7f54a08d7fac598127
Denis M <gdecider@gmail.com>
2017-11-06 12:45:09 +0300

    Вынос настроек в файл конфига

--- mod_conf.php
+++ mod_conf.php
@@ -1,7 +1,53 @@
 <?php if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
-return [
-    'name' => 'LOCAL.MODEXAMPLE',
-    'ns' => 'Local\Modexample',
-    'nsTables' => 'Local\Modexample\Tables',
-    'prefix' => 'local_modexample',
+
+/**
+ * @var array $arCstmProps
+ * пользовательские поля
+ * элемент массива ["объект для добавления поля", "UF_код", "тип", "название"]
+ */
+$arCstmProps = [
+    // кастомные поля пользователя
+    ['USER', 'UF_FIO_DIR', 'string', 'ФИО Директора'],
+    ['USER', 'UF_UR_ADR', 'string', 'Юридический адрес'],
 ];
+
+/**
+ * @var array $arTables
+ * ORM таблицы
+ * Элемент массива - "имя класса таблицы", сама таблица должна быть описана в /lib/tables/
+ */
+$arTables = [
+    'TicketCategory',
+    'TicketStatus',
+    'TicketRating',
+    'TicketCategory',
+    'TicketMsg',
+    'Ticket',
+];
+
+/**
+ * @var array $arIndexes
+ * индексы ORM таблиц
+ * элемент массива - ["имя класса таблицы", "имя поля таблицы"]
+ */
+$arIndexes = [
+    ['Ticket', 'client_id'],
+    ['Ticket', 'manager_id'],
+    ['Ticket', 'ticket_status_id'],
+    ['Ticket', 'ticket_rating_id'],
+    ['Ticket', 'ticket_category_id'],
+    ['TicketMsg', 'ticket_id'],
+    ['TicketMsg', 'sender_id'],
+];
+
+$arConfig = [
+    'name' => 'LOCAL.EXCH1C',
+    'ns' => 'Local\Exch1c',
+    'nsTables' => 'Local\Exch1c\Tables',
+    'prefix' => 'local_exch1c',
+    'arCstmProps' => $arCstmProps,
+    'arTables' => $arTables,
+    'arIndexes' => $arIndexes,
+];
+
+return $arConfig;
