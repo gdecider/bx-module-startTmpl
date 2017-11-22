766b5c1322031b3748a0785f3fb9eaad2d6a7c33
Denis M <gdecider@gmail.com>
2017-11-22 19:44:13 +0300

    Добавлена работа при установке
    Добавлена работа при установке с типами плательщиков, группами свойств, свойствами заказа

--- install/unstep1.php
+++ install/unstep1.php
@@ -25,6 +25,7 @@ $module_id = strtolower($arModConf['name']);
     <p><?=Loc::getMessage("MOD_UNINST_SAVE")?></p>
     <p><input type="checkbox" value="Y" name="savedata" id="savedata" value="Y" checked><label for="savedata"><?=Loc::getMessage("MOD_UNINST_SAVE_TABLES")?></label></p>
     <p><input type="checkbox" value="Y" name="saveprops" id="saveprops" value="Y" checked><label for="saveprops">Сохранить кастомные свойства</label></p>
+    <p><input type="checkbox" value="Y" name="savesaleprops" id="savesaleprops" value="Y" checked><label for="savesaleprops">Сохранить кастомные свойства заказа</label></p>
     <p><input type="checkbox" value="Y" name="saveiblocks" id="saveiblocks" value="Y" checked><label for="saveiblocks">Сохранить инфоблоки</label></p>
     
     <input type="submit" name="" value="<?=Loc::getMessage("MOD_UNINST_DEL")?>">
